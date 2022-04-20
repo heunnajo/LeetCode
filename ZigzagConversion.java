f7a4522d16518592ea6a41009132e7e91bb707e6
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-04-20 14:21:05 +0900

    Update ZigzagConversion.java

--- ZigzagConversion.java
+++ ZigzagConversion.java
@@ -1,23 +1,40 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int len = s.length();
-        char[][] arr = new char[numRows][len/2];
+        //엣지 케이스 처리(백트랙킹)
+        //1.s의 길이 = numRows 2.numRows = 1
+        if(s.length() == numRows) return s;
+        if(numRows == 1) return s;
         
-        int i=0,j=0;
-        for(int idx=0;idx<len;idx++){
-            for(int k=0;k<numRows;k++){
-                arr[i++][j] = s.charAt(idx);
-            }
-            for(int k=0;k<numRows-1;k++){
-                if(j>=len/2) break;;
-                arr[i--][j++] = s.charAt(idx);
+        String[] sArr = s.split("");
+        
+        //각 문자에 대해 반복!
+        int idx = 0;
+        String[] ans = new String[numRows];//정답을 저장하는 String 객체 각 배열값을 ""로 초기화함. 왜지?
+        for(int i=0;i<numRows;i++) ans[i] = "";
+        
+        boolean flag = true;
+        
+        for(String str:sArr){
+            ans[idx] += str;
+            
+            if(flag){
+                if(idx<numRows-1) idx++;
+                else if(idx == numRows-1){
+                    idx--;
+                    flag = false;
+                }
+            } else{
+                if(idx>0) idx--;
+                else if(idx == 0){
+                    idx++;
+                    flag = true;
+                }
             }
         }
+        
         StringBuilder sb = new StringBuilder();
-        for(int row=0;row<arr.length;row++){
-            for(int col=0;col<arr[0].length;col++){
-                sb.append(arr[row][col]);
-            }
+        for(String str:ans){
+            sb.append(str);
         }
         return sb.toString();
     }
