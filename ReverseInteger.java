f3b4dafe5dc66bc7887608f38be7dfeac3e029ac
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-08-28 22:44:43 +0900

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,32 +1,21 @@
 class Solution {
     public int reverse(int x) {
-        String strX = x + "";
-        String reversed = new StringBuilder().append(x).reverse().toString();
-        
-        if(reversed.charAt(reversed.length()-1) == '-'){
-            reversed = "-"+reversed.substring(0,reversed.length()-1);
+        String xs = String.valueOf(x);
+        String rs = new StringBuilder().append(xs).reverse().toString();
+        int result = 0;
+        long tempResult = 0l;
+        if(rs.endsWith("-")) {
+            tempResult = Long.parseLong(rs.substring(0, rs.length()-1)) * -1;
+        } else {
+            tempResult = Long.parseLong(rs);
         }
-        if(reversed.charAt(reversed.length()-1) == '0'){
-            reversed = reversed.substring(1);
+
+        if(tempResult < Integer.MIN_VALUE || tempResult > Integer.MAX_VALUE) {
+            result = 0;
+        } else {
+            result = (int) tempResult;
         }
-        if(Long.parseLong(reversed) >= Integer.MAX_VALUE || Long.parseLong(reversed) <= Integer.MIN_VALUE){
-            return 0;
-        } else{
-            return Long.parseLong(reversed);
-        }
-        /*for(int i=strX.length()-1;i>=0;i--){
-            reversed.append(strX.charAt(i));
-        }
-        String res = reversed.toString();
-        if(strX.charAt(0) == '-'){
-            res = "-"+res.substring(0,strX.length()-1);
-        }
-        if(strX.charAt(strX.length()-1) == '0'){
-            res = res.substring(1);
-        }
-        System.out.println("res: "+res);
-        int ans = Integer.parseInt(res);
-        return ans;*/
-        
+
+        return result;
     }
 }
